f250c03ca2fdb0d1c400283267740bcebfb96da7
itzrossyo <Rossespley@gmail.com>
2023-07-10 23:41:53 +0100

    add chat logic for response

--- script/app.js
+++ script/app.js
@@ -20,6 +20,7 @@ window.addEventListener('DOMContentLoaded', async () => {
 });
 
 // Fetch the data and update the prices
+
 async function updatePrices() {
   const response = await fetch('https://api.coinlore.net/api/tickers/');
   const data = await response.json();
@@ -35,7 +36,12 @@ async function updatePrices() {
 }
 
 // Call the updatePrices function when the DOM is ready
+
 document.addEventListener('DOMContentLoaded', updatePrices);
+
+
+// open and close menu for mobile
+
 
 document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.querySelector(".toggle_btn");
@@ -55,3 +61,65 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+//open and close chat bot
+
+const botImg = document.querySelector(".bot-img");
+const botChatBox = document.querySelector(".bot-chatbot");
+
+botImg.addEventListener("click", function() {
+  botChatBox.style.display = "block";
+});
+
+document.addEventListener("click", function(e) {
+  if (!botImg.contains(e.target) && !botChatBox.contains(e.target)) {
+    botChatBox.style.display = "none";
+  }
+});
+
+//start of chat bot 
+
+const userInput = document.querySelector(".user-input");
+const sendMsg = document.querySelector(".send-btn");
+const messageBox = document.querySelector(".message-box");
+
+sendMsg.addEventListener("click", function () {
+  const inputText = userInput.value;
+  const response = getResponse(inputText);
+  messageBox.innerHTML += `<p>You: ${inputText}</p>`;
+  messageBox.innerHTML += `<p>Bot: ${response}</p>`;
+  userInput.value = "";
+});
+
+// bot logic
+function getResponse(input) {
+  if (input.toLowerCase().includes("hello")) {
+    return "Hello! How can I assist you today?";
+  } else if (input.toLowerCase().includes("help")) {
+    return "Sure, I'm here to help. What do you need assistance with?";
+  } else if (input.toLowerCase().includes("crypto")) {
+    return "Cryptocurrency, or crypto for short, is a digital or virtual form of currency that uses cryptography for security.";
+  } else if (input.toLowerCase().includes("buy")) {
+    return "To buy cryptocurrency,";
+  } else if (input.toLowerCase().includes("sell")) {
+    return "To sell cryptocurrency, you can follow these steps:\n1. Sign in to your cryptocurrency exchange account.\n2. Navigate to the 'Sell' or 'Trade' section of the platform.";
+  } else if (input.toLowerCase().includes("swap")) {
+    return "To swap or exchange cryptocurrencies, ";
+  } else if (input.toLowerCase().includes("get started")) {
+    return "To get started with cryptocurrency, you can follow these steps:";
+  } else if (input.toLowerCase().includes("wallet")) {
+    return "A cryptocurrency wallet is a software program or a physical device that allows you to securely store";
+  } else if (input.toLowerCase().includes("invest")) {
+    return "Investing in cryptocurrency involves purchasing digital assets with the expectation of generating a return over time.";
+  } else if (input.toLowerCase().includes("security")) {
+    return "Security is crucial when dealing with cryptocurrencies. ";
+  } else {
+    return "I'm sorry, I didn't understand your message.";
+  }
+}
+
+
+
+
+//bot respones
+
